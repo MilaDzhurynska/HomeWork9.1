4a34f8a260485df3f89d3885c5e35583ae1843e6
DZHURYNSKA LIUDMYLA <hello.its.me.luda@gmail.com>
2025-02-21 13:14:56 +0200

    fourth commit

--- TwoDimensionalArray.java
+++ TwoDimensionalArray.java
@@ -22,12 +22,13 @@ public class TwoDimensionalArray {
 
         for (int i = 0; i < matrix.length; i++) {
             int sumRows = 0;
-            for (int j = 0; j < matrix[i].length; j++){
-                sumRows += matrix[i][j];}
+            for (int j = 0; j < matrix[i].length; j++) {
+                sumRows += matrix[i][j];
+            }
             if (i % 2 == 0) {
                 sumEvenRows += sumRows; // парні рядки (0; 2)
             } else {
-                 sumOddRows += sumRows; // непарні рядки (1; 3)
+                sumOddRows += sumRows; // непарні рядки (1; 3)
             }
         }
         System.out.println("Сума елементів у парних рядках: " + sumEvenRows);
@@ -38,13 +39,14 @@ public class TwoDimensionalArray {
         та добуток всіх елементів в непарних стовпцях (стовпцях 1, 3).
          */
 
-        int productEvenColumns = 1;
-        int productOddColumns = 1;
+        long productEvenColumns = 1; // краще long, бо може бути > int
+        long productOddColumns = 1; // 1, бо в нас добуток, якщо присвоїти 0, то виведе лише 0
 
         for (int j = 0; j < matrix[0].length; j++) {
-            int productColumns = 1;
+            long productColumns = 1;
             for (int i = 0; i < matrix.length; i++) {
-                productColumns *= matrix[j][i];}
+                productColumns *= matrix[j][i];
+            }
             if (j % 2 == 0) {
                 productEvenColumns *= productColumns; // парні стовпці (0; 2)
             } else {
@@ -53,5 +55,37 @@ public class TwoDimensionalArray {
         }
         System.out.println("Добуток елементів у парних стовпцях: " + productEvenColumns);
         System.out.println("Добуток елементів у непарних стовпцях: " + productOddColumns);
+
+        /*
+        Перевірте, чи матриця є магічним квадратом. Магічний квадрат - це квадратна матриця,
+        в якій суми всіх рядків, стовпців та діагоналей рівні між собою.
+         */
+        int m = 4;
+
+        boolean flag = false; // оголосили Boolean для перевірки магічного квадрата
+
+        int sum1 = 0, sum2 = 0; // ініціалізація та оголошення діагональної суми як 0
+
+        for (int i = 0; i < m; i++){  //знаходження суми двох діагоналей, тобто sum1 і sum2
+            sum1 += matrix[i][i];
+            sum2 += matrix[i][m-1-i];
+        }
+        if (sum1!=sum2) // якщо сума діагоналей нерівна, тоді це не магічний квадрат
+            flag = true;
+
+        for (int i = 0; i < m; i++){
+            int rowSum = 0, colSum = 0;  //Ініціалізація та оголошення суми рядків і стовпців як 0
+            for (int j = 0; j < m; j++){ //знаходження суми рядків і стовпців, тобто рядка і стовпця
+                rowSum += matrix[i][j];
+                colSum += matrix[j][i];
+            }
+            if (rowSum != colSum || colSum != sum1)
+                flag = true;
+
+        }
+        if (!flag)
+            System.out.println("Матриця є магічним квадратом.");
+        else
+            System.out.println("Матриця не є магічним квадратом.");
     }
 }
